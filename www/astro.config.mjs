50e5c8bd3878ee51178675dbffe591bad46e9f5c
m2rain <debil_k2@hotmail.com>
2025-08-05 01:04:26 -0600

    Add Cloudflare Workers Auth port to Docker Compose and set alias for react-dom/server in Astro config for production

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,12 +5,18 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+// https://github.com/withastro/astro/issues/12824
+const alias = import.meta.env.PROD ? {
+  "react-dom/server": "react-dom/server.edge",
+} : undefined;
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: { alias },
   },
 
   adapter: cloudflare({
